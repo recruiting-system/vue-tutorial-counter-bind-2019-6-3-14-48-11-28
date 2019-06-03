7f8c366b94e45cff229f766503b3d1ff43cdfc93
linwenjun <linwenjun@163.com>
2019-06-03 14:48:29 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <button v-on:click="count += 1">+</button>
+    <button v-on:click="count -= 1">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
